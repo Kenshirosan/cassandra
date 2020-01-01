e3a4c2d37d8bb52877f8cf734140bbee0dc1dcf2
Laurent <laurent.sama@gmail.com>
2020-01-01 10:43:58 +0100

    add .env.example

--- client/vue.config.js
+++ client/vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('sass').oneOf(type)))
     },
-}
+};
 
 function addStyleResource (rule) {
     rule.use('style-resource')
