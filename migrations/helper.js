a75fcb38605331c65a3bc064f06e1460eb0f9f4c
Laurent <laurent.sama@gmail.com>
2020-01-04 13:42:48 +0100

    Few tweaks to Migrations

--- migrations/helper.js
+++ migrations/helper.js
@@ -1,8 +1,15 @@
 async function migrate(client, query, funcToRun) {
-    await client.connect().then(() => console.log('Cassandra connected')).catch(e => console.log(e));
+    await client.connect()
+        .then(() => console.log('Cassandra connected'))
+        .catch(e => {
+            console.log(e);
+            process.exit();
+        });
     await funcToRun(client, query);
-    client.shutdown();
-    process.exit();
+    setTimeout(() => {
+        client.shutdown();
+        process.exit()
+    }, 500);
 };
 
 module.exports = migrate;
